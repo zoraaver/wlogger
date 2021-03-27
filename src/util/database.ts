78b07fed0a927bec21fe8d62339259b8b005e7da
zoraaver <zoraaver@gmail.com>
2021-03-27 14:02:38 +0000

    added test uri for db

--- src/util/database.ts
+++ src/util/database.ts
@@ -2,3 +2,4 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const MONGO_URI: string = `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.xgkor.mongodb.net/${process.env.MONGO_DB_NAME}`;
+export const MONGO_TEST_URI: string = MONGO_URI + "_test";
