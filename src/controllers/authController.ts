5a7c9ae947d63009d3170c37dfa8562309372ce4
zoraaver <zoraaver@gmail.com>
2021-03-28 12:51:07 +0100

    adding google auth methods

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,5 +1,9 @@
 import { Request, Response, NextFunction } from "express";
 import User, { userDocument } from "../models/user";
+import { google } from "googleapis";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export async function login(
   req: Request,
@@ -18,3 +22,20 @@ export async function login(
   }
   res.json({ user: { email: user.email, token: user.token } });
 }
+
+export async function googleLogin(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> {
+  const url = oauth2client.generateAuthUrl({
+    access_type: "online",
+    scope: "email",
+  });
+  res.json({ url });
+}
+const oauth2client = new google.auth.OAuth2({
+  clientId: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  redirectUri: `http://localhost:${process.env.PORT || 8080}`,
+});
