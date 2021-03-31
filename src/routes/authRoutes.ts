83476e5e697af2e4929826f2f008a03fc138bd4c
zoraaver <zoraaver@gmail.com>
2021-03-31 19:01:45 +0100

    added validate route

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,7 +1,9 @@
 import express, { Router } from "express";
 import * as authController from "../controllers/authController";
+import { setCurrentUser } from "../middleware/auth";
 
 export const authRoutes: Router = express.Router();
 
 authRoutes.post("/login", authController.login);
 authRoutes.post("/google", authController.googleLogin);
+authRoutes.post("/validate", setCurrentUser, authController.validate);
