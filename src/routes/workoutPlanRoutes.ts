bdfc4e23f70a5141515d6b3d1de9ce9a552f15cc
zoraaver <zoraaver@gmail.com>
2021-04-07 19:30:26 +0100

    added crud routes

--- src/routes/workoutPlanRoutes.ts
+++ src/routes/workoutPlanRoutes.ts
@@ -4,3 +4,8 @@ import * as workoutPlansController from "../controllers/workoutPlansController";
 export const workoutPlanRoutes: Router = Router();
 
 workoutPlanRoutes.post("/", workoutPlansController.create);
+workoutPlanRoutes.get("/", workoutPlansController.index);
+workoutPlanRoutes.get("/:id", workoutPlansController.show);
+workoutPlanRoutes.patch("/:id", workoutPlansController.update);
+workoutPlanRoutes.put("/:id", workoutPlansController.update);
+workoutPlanRoutes.delete("/:id", workoutPlansController.destroy);
