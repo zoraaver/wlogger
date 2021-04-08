ce0de6694439076def1843f4c57817e61a1b1c6b
zoraaver <zoraaver@gmail.com>
2021-04-08 14:34:22 +0100

    changed to local mongodb to speed up tests

--- src/util/database.ts
+++ src/util/database.ts
@@ -4,5 +4,8 @@ import {
   MONGO_DB_PASSWORD,
 } from "../../keys.json";
 
-export const MONGO_URI: string = `mongodb+srv://${MONGO_DB_USER}:${MONGO_DB_PASSWORD}@cluster0.xgkor.mongodb.net/${MONGO_DB_NAME}`;
+const MONGO_DB_CREDENTIALS: string =
+  MONGO_DB_USER === "" ? "" : `${MONGO_DB_USER}:${MONGO_DB_PASSWORD}@`;
+
+export const MONGO_URI: string = `mongodb://${MONGO_DB_CREDENTIALS}localhost:27017/${MONGO_DB_NAME}`;
 export const MONGO_TEST_URI: string = MONGO_URI + "_test";
