31ac749369accc587198f10dcf78aa68254be160
zoraaver <zoraaver@gmail.com>
2021-04-10 17:26:59 +0100

    added id validation for mongo id's

---
+++ src/middleware/workoutPlan.ts
@@ -0,0 +1,15 @@
+import { Request, Response, NextFunction } from "express";
+import { isValidObjectId } from "mongoose";
+
+export function validateWorkoutPlanId(
+  req: Request<{ id?: string }>,
+  res: Response,
+  next: NextFunction
+) {
+  const { id } = req.params;
+  if (!id || !isValidObjectId(id)) {
+    res.status(406).json({ message: `${id} is an invalid id` });
+    return;
+  }
+  next();
+}
--- src/routes/workoutPlanRoutes.ts
+++ src/routes/workoutPlanRoutes.ts
@@ -1,11 +1,28 @@
 import { Router } from "express";
 import * as workoutPlansController from "../controllers/workoutPlansController";
+import { validateWorkoutPlanId } from "../middleware/workoutPlan";
 
 export const workoutPlanRoutes: Router = Router();
 
 workoutPlanRoutes.post("/", workoutPlansController.create);
 workoutPlanRoutes.get("/", workoutPlansController.index);
-workoutPlanRoutes.get("/:id", workoutPlansController.show);
-workoutPlanRoutes.patch("/:id", workoutPlansController.update);
-workoutPlanRoutes.put("/:id", workoutPlansController.update);
-workoutPlanRoutes.delete("/:id", workoutPlansController.destroy);
+workoutPlanRoutes.get(
+  "/:id",
+  validateWorkoutPlanId,
+  workoutPlansController.show
+);
+workoutPlanRoutes.patch(
+  "/:id",
+  validateWorkoutPlanId,
+  workoutPlansController.update
+);
+workoutPlanRoutes.put(
+  "/:id",
+  validateWorkoutPlanId,
+  workoutPlansController.update
+);
+workoutPlanRoutes.delete(
+  "/:id",
+  validateWorkoutPlanId,
+  workoutPlansController.destroy
+);
