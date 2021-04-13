4377c06c97c9613ee31fdafd93180d8a1ce2009c
zoraaver <zoraaver@gmail.com>
2021-04-13 17:23:56 +0100

    added index implementation

--- src/controllers/workoutLogsController.ts
+++ src/controllers/workoutLogsController.ts
@@ -1,7 +1,19 @@
 import { Request, Response } from "express";
-import { ResponseError } from "../../@types";
+import { LeanDocument } from "mongoose";
+import { ResponseError, ResponseMessage } from "../../@types";
 import { User, userDocument } from "../models/user";
-import { WorkoutLog, workoutLogDocument } from "../models/workoutLog";
+import {
+  exerciseData,
+  WorkoutLog,
+  workoutLogDocument,
+} from "../models/workoutLog";
+
+interface workoutLogHeaderData {
+  createdAt: Date;
+  setCount: number;
+  exerciseCount: number;
+  _id: string;
+}
 
 export async function create(
   req: Request,
@@ -10,7 +22,7 @@ export async function create(
   try {
     const workoutLog: workoutLogDocument = await WorkoutLog.create(req.body);
     const user: userDocument | null = await User.findById(req.currentUserId);
-    user?.workoutLogs.push(workoutLog._id);
+    user?.workoutLogs.unshift(workoutLog._id);
     await user?.save();
     res.status(201).json(workoutLog);
   } catch (error) {
@@ -18,4 +30,37 @@ export async function create(
   }
 }
 
-export async function index(req: Request, res: Response): Promise<void> {}
+export async function index(
+  req: Request,
+  res: Response<workoutLogHeaderData[] | ResponseMessage>
+): Promise<void> {
+  const user: LeanDocument<userDocument> | null = await User.findById(
+    req.currentUserId,
+    "workoutLogs"
+  )
+    .lean()
+    .populate("workoutLogs");
+  if (user) {
+    res.json(generateWorkoutLogHeaderData(user.workoutLogs));
+  } else {
+    res.status(500).json({ message: "An error occurred" });
+  }
+}
+
+function calculateSetNumber(exercises: exerciseData[]): number {
+  return exercises.reduce(
+    (total: number, curr: exerciseData) => total + curr.sets.length,
+    0
+  );
+}
+
+function generateWorkoutLogHeaderData(
+  workoutLogs: workoutLogDocument[]
+): workoutLogHeaderData[] {
+  return workoutLogs.map(
+    ({ createdAt, exercises, _id }: workoutLogDocument) => {
+      const setCount = calculateSetNumber(exercises);
+      return { createdAt, setCount, exerciseCount: exercises.length, _id };
+    }
+  );
+}
