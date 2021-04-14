c92862d1d749d10508f4721a1bc5c871328b4912
zoraaver <zoraaver@gmail.com>
2021-04-14 12:09:13 +0100

    changed middleware name func to validateId

--- src/routes/workoutPlanRoutes.ts
+++ src/routes/workoutPlanRoutes.ts
@@ -1,28 +1,12 @@
 import { Router } from "express";
 import * as workoutPlansController from "../controllers/workoutPlansController";
-import { validateWorkoutPlanId } from "../middleware/workoutPlan";
+import { validateId } from "../middleware/middleware";
 
 export const workoutPlanRoutes: Router = Router();
 
 workoutPlanRoutes.post("/", workoutPlansController.create);
 workoutPlanRoutes.get("/", workoutPlansController.index);
-workoutPlanRoutes.get(
-  "/:id",
-  validateWorkoutPlanId,
-  workoutPlansController.show
-);
-workoutPlanRoutes.patch(
-  "/:id",
-  validateWorkoutPlanId,
-  workoutPlansController.update
-);
-workoutPlanRoutes.put(
-  "/:id",
-  validateWorkoutPlanId,
-  workoutPlansController.update
-);
-workoutPlanRoutes.delete(
-  "/:id",
-  validateWorkoutPlanId,
-  workoutPlansController.destroy
-);
+workoutPlanRoutes.get("/:id", validateId, workoutPlansController.show);
+workoutPlanRoutes.patch("/:id", validateId, workoutPlansController.update);
+workoutPlanRoutes.put("/:id", validateId, workoutPlansController.update);
+workoutPlanRoutes.delete("/:id", validateId, workoutPlansController.destroy);
