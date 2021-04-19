d4dfff08a0415b9570631630899805087b94c6c2
zoraaver <zoraaver@gmail.com>
2021-04-19 16:29:38 +0100

    added start route

--- src/routes/workoutPlanRoutes.ts
+++ src/routes/workoutPlanRoutes.ts
@@ -4,9 +4,10 @@ import { validateId } from "../middleware/middleware";
 
 export const workoutPlanRoutes: Router = Router();
 
-workoutPlanRoutes.post("/", workoutPlansController.create);
-workoutPlanRoutes.get("/", workoutPlansController.index);
+workoutPlanRoutes.patch("/start/:id", validateId, workoutPlansController.start);
 workoutPlanRoutes.get("/:id", validateId, workoutPlansController.show);
 workoutPlanRoutes.patch("/:id", validateId, workoutPlansController.update);
 workoutPlanRoutes.put("/:id", validateId, workoutPlansController.update);
 workoutPlanRoutes.delete("/:id", validateId, workoutPlansController.destroy);
+workoutPlanRoutes.post("/", workoutPlansController.create);
+workoutPlanRoutes.get("/", workoutPlansController.index);
