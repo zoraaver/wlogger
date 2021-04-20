5e328fa5dbd091162c0f18982a35379e48f4313f
zoraaver <zoraaver@gmail.com>
2021-04-20 12:53:59 +0100

    export daysToNumbers map

--- src/models/workout.ts
+++ src/models/workout.ts
@@ -12,16 +12,26 @@ export type Day =
 export type weightUnit = "kg" | "lb";
 
 export const days: Day[] = [
+  "Sunday",
   "Monday",
   "Tuesday",
   "Wednesday",
   "Thursday",
   "Friday",
   "Saturday",
-  "Sunday",
 ];
 
 export const weightUnits: weightUnit[] = ["kg", "lb"];
+
+export const daysToNumbers: { [dayOfWeek: string]: number } = {
+  Monday: 0,
+  Tuesday: 1,
+  Wednesday: 2,
+  Thursday: 3,
+  Friday: 4,
+  Saturday: 5,
+  Sunday: 6,
+};
 
 export interface workoutDocument extends Document {
   dayOfWeek: Day;
