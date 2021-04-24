4c847624bea827c394b18a489a5674c32bb0a1df
zoraaver <zoraaver@gmail.com>
2021-04-24 18:38:50 +0100

    Add id validation to routes

--- src/routes/workoutLogRoutes.ts
+++ src/routes/workoutLogRoutes.ts
@@ -1,10 +1,17 @@
 import { Router } from "express";
 import * as workoutLogsController from "../controllers/workoutLogsController";
-import { validateId } from "../middleware/middleware";
+import { validateWorkoutLogId } from "../middleware/workoutLog";
 
 export const workoutLogRoutes: Router = Router();
 
 workoutLogRoutes.post("/", workoutLogsController.create);
-workoutLogRoutes.get("/:id", validateId, workoutLogsController.show);
+
+workoutLogRoutes.get("/:id", validateWorkoutLogId, workoutLogsController.show);
+
 workoutLogRoutes.get("/", workoutLogsController.index);
-workoutLogRoutes.delete("/:id", validateId, workoutLogsController.destroy);
+
+workoutLogRoutes.delete(
+  "/:id",
+  validateWorkoutLogId,
+  workoutLogsController.destroy
+);
--- src/routes/workoutPlanRoutes.ts
+++ src/routes/workoutPlanRoutes.ts
@@ -1,15 +1,62 @@
 import { Router } from "express";
 import * as workoutPlansController from "../controllers/workoutPlansController";
-import { validateId } from "../middleware/middleware";
+import {
+  updateCurrentPlanAsCompletedIfNecessary,
+  validateWorkoutPlanId,
+} from "../middleware/workoutPlan";
 
 export const workoutPlanRoutes: Router = Router();
 
 workoutPlanRoutes.post("/", workoutPlansController.create);
-workoutPlanRoutes.patch("/start/:id", validateId, workoutPlansController.start);
-workoutPlanRoutes.patch("/:id", validateId, workoutPlansController.update);
-workoutPlanRoutes.put("/:id", validateId, workoutPlansController.update);
-workoutPlanRoutes.delete("/:id", validateId, workoutPlansController.destroy);
-workoutPlanRoutes.get("/nextWorkout", workoutPlansController.nextWorkout);
-workoutPlanRoutes.get("/current", workoutPlansController.current);
-workoutPlanRoutes.get("/:id", validateId, workoutPlansController.show);
-workoutPlanRoutes.get("/", workoutPlansController.index);
+
+workoutPlanRoutes.patch(
+  "/start/:id",
+  validateWorkoutPlanId,
+  updateCurrentPlanAsCompletedIfNecessary,
+  workoutPlansController.start
+);
+
+workoutPlanRoutes.patch(
+  "/:id",
+  validateWorkoutPlanId,
+  updateCurrentPlanAsCompletedIfNecessary,
+  workoutPlansController.update
+);
+
+workoutPlanRoutes.put(
+  "/:id",
+  validateWorkoutPlanId,
+  updateCurrentPlanAsCompletedIfNecessary,
+  workoutPlansController.update
+);
+
+workoutPlanRoutes.delete(
+  "/:id",
+  validateWorkoutPlanId,
+  workoutPlansController.destroy
+);
+
+workoutPlanRoutes.get(
+  "/nextWorkout",
+  updateCurrentPlanAsCompletedIfNecessary,
+  workoutPlansController.nextWorkout
+);
+
+workoutPlanRoutes.get(
+  "/current",
+  updateCurrentPlanAsCompletedIfNecessary,
+  workoutPlansController.current
+);
+
+workoutPlanRoutes.get(
+  "/:id",
+  validateWorkoutPlanId,
+  updateCurrentPlanAsCompletedIfNecessary,
+  workoutPlansController.show
+);
+
+workoutPlanRoutes.get(
+  "/",
+  updateCurrentPlanAsCompletedIfNecessary,
+  workoutPlansController.index
+);
