a7a21cf5c985191443785d9092f3789885f683e7
zoraaver <zoraaver@gmail.com>
2021-05-03 20:38:35 +0100

    Rearrange routes by HTTP verb

--- src/routes/workoutLogRoutes.ts
+++ src/routes/workoutLogRoutes.ts
@@ -8,29 +8,37 @@ import { validateWorkoutLogId } from "../middleware/workoutLog";
 
 export const workoutLogRoutes: Router = Router();
 
+workoutLogRoutes.post(
+  "/:id/videoUpload",
+  validateWorkoutLogId,
+  setCurrentWorkoutLog,
+  S3WorkoutLogVideoUpload.array("formVideos", 5),
+  workoutLogsController.uploadSetVideo
+);
+
 workoutLogRoutes.post("/", workoutLogsController.create);
+
+workoutLogRoutes.get(
+  "/:id/exercises/:exerciseId/sets/:setId/video",
+  validateWorkoutLogId,
+  setCurrentWorkoutLog,
+  workoutLogsController.showSetVideo
+);
 
 workoutLogRoutes.get("/:id", validateWorkoutLogId, workoutLogsController.show);
 
 workoutLogRoutes.get("/", workoutLogsController.index);
 
 workoutLogRoutes.delete(
+  "/:id/exercises/:exerciseId/sets/:setId/",
+  validateWorkoutLogId,
+  setCurrentWorkoutLog,
+  workoutLogsController.destroySetVideo
+);
+
+workoutLogRoutes.delete(
   "/:id",
   validateWorkoutLogId,
+  setCurrentWorkoutLog,
   workoutLogsController.destroy
-);
-
-workoutLogRoutes.get(
-  "/:id/:exerciseIndex/:setIndex/stream",
-  validateWorkoutLogId,
-  setCurrentWorkoutLog,
-  workoutLogsController.videoDownload
-);
-
-workoutLogRoutes.post(
-  "/:id/videoUpload",
-  validateWorkoutLogId,
-  setCurrentWorkoutLog,
-  S3WorkoutLogVideoUpload.array("formVideos", 5),
-  workoutLogsController.videoUpload
 );
