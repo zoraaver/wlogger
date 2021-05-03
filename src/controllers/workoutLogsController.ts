e7aa59eaddb3dbbca1f8135f1859ba69c9dcb3f5
zoraaver <zoraaver@gmail.com>
2021-05-03 20:36:23 +0100

    Add actions to show/delete a set video. Change upload video action to use id's rather than indices. Alter destroy action to remove all associated videos in S3 bucket.

--- src/controllers/workoutLogsController.ts
+++ src/controllers/workoutLogsController.ts
@@ -1,15 +1,16 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import { ResponseError, ResponseMessage } from "../../@types";
 import { userDocument } from "../models/user";
 import {
+  loggedSet,
   videoFileExtension,
   WorkoutLog,
   workoutLogDocument,
   workoutLogHeaderData,
 } from "../models/workoutLog";
-import { ReadStream } from "s3-streams";
 import { S3 } from "../config/aws";
 import { WLOGGER_BUCKET } from "../../keys.json";
+import { pipeline } from "stream";
 
 export async function create(
   req: Request,
@@ -52,30 +53,39 @@ export async function show(
 }
 
 export async function destroy(
-  req: Request<{ id: string }>,
+  req: Request,
   res: Response<string>
 ): Promise<void> {
-  const { id } = req.params;
   const user = req.currentUser as userDocument;
+  const workoutLogToDelete = req.currentWorkoutLog as workoutLogDocument;
   const workoutLogToDeleteIndex:
     | number
     | undefined = user.workoutLogs.findIndex(
-    (workoutLog: workoutLogDocument) => workoutLog.toString() === id
+    (workoutLog: workoutLogDocument) =>
+      workoutLog.toString() === workoutLogToDelete.id
   );
   user.workoutLogs.splice(workoutLogToDeleteIndex, 1);
-  await Promise.all([user.save(), WorkoutLog.findByIdAndDelete(id)]);
-  res.json(id);
+  await Promise.all([
+    workoutLogToDelete.deleteAllSetVideos(user.id),
+    user.save(),
+    workoutLogToDelete.delete(),
+  ]);
+  res.json(workoutLogToDelete.id);
 }
 
-export async function videoUpload(req: Request, res: Response): Promise<void> {
+export async function uploadSetVideo(
+  req: Request,
+  res: Response
+): Promise<void> {
   const workoutLog = req.currentWorkoutLog as workoutLogDocument;
   for (let i = 0; i < req.files.length; ++i) {
     const file: Express.Multer.File = (req.files as Express.Multer.File[])[i];
     const fileParts: string[] = file.originalname.split(".");
-    const exerciseIndex = Number(fileParts[0]);
-    const setIndex = Number(fileParts[1]);
-    const fileExtension = fileParts[2];
-    workoutLog.exercises[exerciseIndex].sets[setIndex].formVideo = {
+    const [exerciseId, setId, fileExtension] = fileParts;
+    const set = workoutLog.exercises
+      .find((exercise) => exercise.id === exerciseId)
+      ?.sets.find((set) => set.id === setId) as loggedSet;
+    set.formVideo = {
       size: file.size,
       extension: fileExtension as videoFileExtension,
     };
@@ -84,28 +94,75 @@ export async function videoUpload(req: Request, res: Response): Promise<void> {
   res.json();
 }
 
-export async function videoDownload(
-  req: Request<{ id: string; setIndex: string; exerciseIndex: string }>,
-  res: Response
+export async function showSetVideo(
+  req: Request<{ setId: string; exerciseId: string; id: string }>,
+  res: Response,
+  next: NextFunction
 ): Promise<void> {
   const workoutLog = req.currentWorkoutLog as workoutLogDocument;
-  const exerciseIndex = Number(req.params.exerciseIndex);
-  const setIndex = Number(req.params.setIndex);
-  if (
-    !workoutLog.isValidExerciseIndex(exerciseIndex) ||
-    !workoutLog.isValidSetIndex(setIndex, exerciseIndex) ||
-    !workoutLog.exercises[exerciseIndex].sets[setIndex].formVideo
-  ) {
+  const { setId, exerciseId } = req.params;
+  const set: loggedSet | undefined = workoutLog.findSet(exerciseId, setId);
+  if (!set || !set.formVideo) {
     res.status(404).json();
     return;
   }
-  const fileExtension: videoFileExtension | undefined =
-    workoutLog.exercises[exerciseIndex].sets[setIndex].formVideo?.extension;
-  const videoKey: string = `${req.currentUser?.id}/${workoutLog.id}/${exerciseIndex}.${setIndex}.${fileExtension}`;
+  const fileExtension: videoFileExtension | undefined = set.formVideo.extension;
+  const displayFileName = workoutLog.generateSetVideoDisplayFileName(
+    exerciseId,
+    setId
+  );
+  res.attachment(displayFileName);
+  res.contentType(fileExtension);
+  if (req.headers.range)
+    writeVideoStreamHeaders(res, set.formVideo.size, req.headers.range);
 
-  const src: ReadStream = new ReadStream(S3, {
+  const videoKey: string = `${req.currentUser?.id}/${workoutLog.id}/${exerciseId}.${setId}.${fileExtension}`;
+  const src = S3.getObject({
     Bucket: WLOGGER_BUCKET,
     Key: videoKey,
-  });
-  src.pipe(res);
+    Range: req.headers.range,
+  }).createReadStream();
+
+  pipeline(src, res, (err: NodeJS.ErrnoException | null) => {});
+}
+
+function writeVideoStreamHeaders(
+  res: Response,
+  totalFileSize: number,
+  range: string
+): void {
+  // example range header: bytes=100-300
+  const bytes = range.replace(/bytes=/, "").split("-");
+  const rangeStart: number = parseInt(bytes[0], 10);
+  const rangeEnd: number = bytes[1]
+    ? parseInt(bytes[1], 10)
+    : totalFileSize - 1;
+  const chunkSize: number = rangeEnd - rangeStart + 1;
+
+  const headers = {
+    "Accept-Ranges": "bytes",
+    "Content-Length": chunkSize,
+    "Content-Range": `bytes ${rangeStart}-${rangeEnd}/${totalFileSize}`,
+    "Content-Disposition": "inline",
+  };
+
+  res.writeHead(206, headers);
+}
+
+export async function destroySetVideo(
+  req: Request<{ setId: string; exerciseId: string; id: string }>,
+  res: Response<{ setId: string; exerciseId: string }>
+): Promise<void> {
+  const workoutLog = req.currentWorkoutLog as workoutLogDocument;
+  const { setId, exerciseId } = req.params;
+  const videoDeleted: boolean = await workoutLog.deleteSetVideo(
+    exerciseId,
+    setId,
+    req.currentUser?.id
+  );
+  if (!videoDeleted) {
+    res.status(404).json();
+    return;
+  }
+  res.json({ setId, exerciseId });
 }
