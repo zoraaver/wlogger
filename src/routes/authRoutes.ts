1b5be015b86fa3c03f3e6ad76d9f12516713601b
zoraaver <zoraaver@gmail.com>
2021-06-10 14:49:29 +0100

    Add /auth/apple route

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -6,6 +6,7 @@ export const authRoutes: Router = express.Router();
 
 authRoutes.post("/login", authController.login);
 authRoutes.post("/google", authController.googleLogin);
+authRoutes.post("/apple", authController.appleLogin);
 authRoutes.get("/validate", setCurrentUser, authController.validate);
 authRoutes.post("/verify", authController.verify);
 authRoutes.get("/logout", authController.logout);
