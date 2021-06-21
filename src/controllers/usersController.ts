1b9224ada77f93b239e123b2dbd7421a8b369f1a
zoraaver <zoraaver@gmail.com>
2021-06-21 16:24:17 +0100

    Move sendVerificationEmail logic inside user model

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -1,13 +1,5 @@
 import { Request, Response } from "express";
-import sgMail from "@sendgrid/mail";
-import {
-  SENDGRID_KEY,
-  CLIENT_URL,
-  VERIFICATION_EMAIL_TEMPLATE_ID,
-} from "../config/env";
 import { userDocument, User } from "../models/user";
-
-sgMail.setApiKey(SENDGRID_KEY);
 
 export async function create(req: Request, res: Response): Promise<void> {
   const { email, password, confirmPassword } = req.body;
@@ -16,41 +8,29 @@ export async function create(req: Request, res: Response): Promise<void> {
     let user: userDocument | null = await User.findOne({
       email: email?.toLowerCase(),
     });
+
     if (user) {
       throw new Error(
         "Email validation error: email: Email has already been taken"
       );
     }
-    if (password != confirmPassword)
+
+    if (password != confirmPassword) {
       throw new Error(
         "User validation error: confirmPassword: Confirm password does not match password"
       );
+    }
 
     user = new User({ email, password });
     await user.save();
-    sendVerificationEmail(email, user.getVerificationToken());
+
+    user.sendVerificationEmail();
+
     res.status(201).json({ user: { email: user.email } });
   } catch (error) {
     const errorMessage: string = error.message.split(": ")[2];
     const field: string = error.message.split(": ")[1];
-    res.status(406).json({ field, error: errorMessage });
-    return;
-  }
-}
 
-async function sendVerificationEmail(
-  email: string,
-  token: string
-): Promise<void> {
-  const verifyLink: string = `${CLIENT_URL}/verify/${token}`;
-  try {
-    await sgMail.send({
-      from: { email: "app@wlogger.uk", name: "wLogger" },
-      to: email,
-      dynamicTemplateData: { verifyLink },
-      templateId: VERIFICATION_EMAIL_TEMPLATE_ID,
-    });
-  } catch (error) {
-    console.error(error);
+    res.status(406).json({ field, error: errorMessage });
   }
 }
