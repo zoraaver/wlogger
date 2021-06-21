333056bfc6461f692fe0436943c440f643d0fdca
zoraaver <zoraaver@gmail.com>
2021-06-21 18:46:18 +0100

    Add day types/constants and getCurrentWeekday function

--- src/util/util.ts
+++ src/util/util.ts
@@ -2,6 +2,21 @@
 export const millisecondsInDay: number = 1000 * 60 * 60 * 24;
 export const millisecondsInWeek: number = 1000 * 60 * 60 * 24 * 7;
 export const megaByte: number = 1000000;
+export const weightUnits = ["kg", "lb"] as const;
+export enum daysToNumbers {
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+  Sunday,
+}
+
+export const days = Object.keys(daysToNumbers).slice(-7);
+
+export type weightUnit = typeof weightUnits[number];
+export type Day = keyof typeof daysToNumbers;
 
 export function dateDifferenceInWeeks(d1: Date, d2: Date): number {
   return Math.floor(
@@ -9,6 +24,12 @@ export function dateDifferenceInWeeks(d1: Date, d2: Date): number {
       Date.UTC(d1.getFullYear(), d1.getMonth(), d1.getDate())) /
       millisecondsInWeek
   );
+}
+
+export function getCurrentWeekDay(): Day {
+  return new Intl.DateTimeFormat("en-us", { weekday: "long" }).format(
+    new Date(Date.now())
+  ) as Day;
 }
 
 export function goBackToPreviousMonday(date: Date): Date {
