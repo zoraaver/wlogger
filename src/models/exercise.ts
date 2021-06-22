55e5dd010cef85da0d63589e792050d187f886d0
zoraaver <zoraaver@gmail.com>
2021-06-22 20:22:12 +0100

    Add notes fields and validations for name and notes fields

--- src/models/exercise.ts
+++ src/models/exercise.ts
@@ -1,13 +1,27 @@
 import { Document, model, Schema } from "mongoose";
 
-export interface exerciseDocument extends Document {
+interface exercise {
   name: string;
-  category: string[];
+  notes?: string;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
-const exerciseSchema = new Schema<exerciseDocument>({
-  name: { type: String, required: true },
-  categories: [String],
-});
+export type exerciseDocument = exercise & Document;
 
-export default model<exerciseDocument>("Exercise", exerciseSchema);
+const exerciseSchema = new Schema<exerciseDocument>(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is a required field"],
+      maxLength: [100, "Name must be at most 100 characters"],
+    },
+    notes: {
+      type: String,
+      maxLength: [500, "Notes must be at most 500 characters"],
+    },
+  },
+  { timestamps: true }
+);
+
+export const Exercise = model<exerciseDocument>("Exercise", exerciseSchema);
