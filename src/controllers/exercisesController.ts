6131a3ae28c422f7f41cf6a0927cf2ae72a5a805
zoraaver <zoraaver@gmail.com>
2021-06-23 16:23:36 +0100

    Add index and destroy actions

--- src/controllers/exercisesController.ts
+++ src/controllers/exercisesController.ts
@@ -3,8 +3,6 @@ import { ResponseError } from "../../@types";
 import { Exercise, exerciseDocument } from "../models/exercise";
 import { userDocument } from "../models/user";
 
-export async function index(req: Request, res: Response): Promise<void> {}
-
 export async function create(
   req: Request<any, any, { notes?: string; name: string }>,
   res: Response<exerciseDocument | ResponseError>
@@ -21,4 +19,35 @@ export async function create(
     const [, field, message]: string[] = error.message.split(": ");
     res.status(406).json({ field, error: message });
   }
+}
+
+export async function index(
+  req: Request,
+  res: Response<exerciseDocument[]>
+): Promise<void> {
+  const user = req.currentUser as userDocument;
+
+  await user
+    .populate({ path: "exercises", options: { sort: { name: 1 } } })
+    .execPopulate();
+
+  res.json(user.exercises);
+}
+
+export async function destroy(
+  req: Request<{ id: string }>,
+  res: Response<string>
+): Promise<void> {
+  const { id } = req.params;
+  const user = req.currentUser as userDocument;
+
+  const exerciseIndex: number | undefined = user.exercises.findIndex(
+    (exercise: exerciseDocument) => exercise.id === id
+  );
+
+  user.exercises.splice(exerciseIndex, 1);
+
+  await Promise.all([user.save(), Exercise.findByIdAndDelete(id)]);
+
+  res.json(id);
 }
